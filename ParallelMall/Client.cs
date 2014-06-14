d791123a8bd14874352a51f7baa49738690835f7
Michał Cywiński <mcywinski@hotmail.com>
2014-06-14 23:33:53 +0200

    Brakujący plik definicji klienta

---
+++ ParallelMall/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParallelMall
+{
+    class Client
+    {
+        public int ProductType { get; set; } //produkt, którym zainteresowany jest klient
+
+        public Client(int productType)
+        {
+            ProductType = productType;
+        }
+    }
+}
